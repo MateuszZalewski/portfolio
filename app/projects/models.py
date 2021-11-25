c360de29e48687fbab55f20e28fc444c03c03652
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-11-25 22:13:44 +0100

    add tech model

--- app/projects/models.py
+++ app/projects/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 
+class Tech(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    logo = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    github = models.URLField(null=True)
-    link = models.URLField(null=True)
+    github = models.URLField(blank=True)
+    link = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
