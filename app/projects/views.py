ee873bccb0c894727cc01b1aa6ea329aaae36914
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-11-26 14:55:36 +0100

    add project-list view

--- app/projects/views.py
+++ app/projects/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Project
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'projects/list.html'
+    context_object_name = 'projects'
