e2681c42e88abc6abaac2df327e7d4255a13b7dc
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-11-26 15:07:16 +0100

    override save in Tech model to automatically resize logo

--- app/projects/models.py
+++ app/projects/models.py
@@ -11,6 +11,17 @@ class Tech(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super().save(force_insert=force_insert, force_update=force_update,
+                     using=using, update_fields=update_fields)
+
+        img = Image.open(self.logo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.logo.path)
+
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
